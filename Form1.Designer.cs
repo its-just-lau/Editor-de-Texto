c85e535b951e637afcf5ca4ed2b82aeb51f0b28a
its-just-lau <itsjustlau2580@gmail.com>
2025-02-12 11:17:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Editor_de_Texto
+{
+    partial class Editor
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Editor));
+            this.menuEditor = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbEditor = new System.Windows.Forms.RichTextBox();
+            this.openFileDialogEditor = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogEditor = new System.Windows.Forms.SaveFileDialog();
+            this.menuEditor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuEditor
+            // 
+            this.menuEditor.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuEditor.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuEditor.Location = new System.Drawing.Point(0, 0);
+            this.menuEditor.Name = "menuEditor";
+            this.menuEditor.Size = new System.Drawing.Size(800, 28);
+            this.menuEditor.TabIndex = 0;
+            this.menuEditor.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoToolStripMenuItem,
+            this.abrirToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("archivoToolStripMenuItem.Image")));
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(93, 24);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("nuevoToolStripMenuItem.Image")));
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("abrirToolStripMenuItem.Image")));
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("guardarToolStripMenuItem.Image")));
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("guardarComoToolStripMenuItem.Image")));
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.guardarComoToolStripMenuItem.Text = "Guardar como...";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("salirToolStripMenuItem.Image")));
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // rtbEditor
+            // 
+            this.rtbEditor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbEditor.Location = new System.Drawing.Point(0, 28);
+            this.rtbEditor.Name = "rtbEditor";
+            this.rtbEditor.Size = new System.Drawing.Size(800, 422);
+            this.rtbEditor.TabIndex = 1;
+            this.rtbEditor.Text = "";
+            this.rtbEditor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rtbEditor_KeyPress);
+            // 
+            // openFileDialogEditor
+            // 
+            this.openFileDialogEditor.FileName = "openFileDialog1";
+            // 
+            // Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtbEditor);
+            this.Controls.Add(this.menuEditor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuEditor;
+            this.Name = "Editor";
+            this.Text = "Editor de Texto";
+            this.menuEditor.ResumeLayout(false);
+            this.menuEditor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuEditor;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox rtbEditor;
+        private System.Windows.Forms.OpenFileDialog openFileDialogEditor;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogEditor;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Editor_de_Texto
+{
+    public partial class Editor : Form
+    {
+        bool archivoGuardado = false;
+        string filePath = null;
+        bool cambiosRealizados = false;
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rtbEditor.Text != "" || archivoGuardado) {
+                DialogResult resultado;
+                resultado = MessageBox.Show("Desea guardar antes de abrir un nuevo documento?",
+                    "Sistema", MessageBoxButtons.YesNoCancel);
+                switch (resultado) {                       
+                    case DialogResult.Yes:
+                        guardarToolStripMenuItem_Click(sender,e);
+                        archivoGuardado = false;
+                        filePath = null;
+                        rtbEditor.Clear();
+                        break;
+                     case DialogResult.No:
+                        archivoGuardado = false;
+                        filePath = null;
+                        rtbEditor.Clear();
+                        break;
+
+                }
+
+            }
+            else
+            {
+                archivoGuardado = false;
+                filePath = null;
+                rtbEditor.Clear();
+            }
+
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = openFileDialogEditor.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = openFileDialogEditor.FileName;
+                try
+                {
+                    string texto = File.ReadAllText(filePath);
+                    rtbEditor.Text = texto;
+                    archivoGuardado = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el archivo: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            if (archivoGuardado == false)
+            {
+                resultado = saveFileDialogEditor.ShowDialog();
+                if (resultado == DialogResult.OK)
+                {
+                    filePath = saveFileDialogEditor.FileName;
+                    string texto = rtbEditor.Text;
+                    try
+                    {
+                        File.WriteAllText(filePath, texto);
+                        MessageBox.Show("Archivo guardado correctamente");
+                        archivoGuardado = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    string texto = rtbEditor.Text;
+                    File.WriteAllText(filePath, texto);
+                    MessageBox.Show("Archivo guardado correctamente");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            filePath = "";
+            resultado = saveFileDialogEditor.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = saveFileDialogEditor.FileName;
+                string texto = rtbEditor.Text;
+                try
+                {
+                    File.WriteAllText(filePath, texto);
+                    MessageBox.Show("Archivo guardado correctamente");
+                    archivoGuardado = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (archivoGuardado && cambiosRealizados) {
+                DialogResult resultado;
+                resultado = MessageBox.Show("Desea guardar antes de salir?",
+                    "Sistema", MessageBoxButtons.YesNoCancel);
+                switch (resultado)
+                {
+                    case DialogResult.Yes:
+                        guardarToolStripMenuItem_Click(sender, e);
+                        this.Close();
+                        break;
+                    case DialogResult.No:
+                        this.Close();
+                        break;
+
+                }
+            }
+            else if (cambiosRealizados && !archivoGuardado) 
+            {
+                DialogResult resultado;
+                resultado = MessageBox.Show("Desea guardar antes de salir?",
+                    "Sistema", MessageBoxButtons.YesNoCancel);
+                switch (resultado)
+                {
+                    case DialogResult.Yes:
+                        guardarToolStripMenuItem_Click(sender, e);
+                        this.Close();
+                        break;
+                    case DialogResult.No:
+                        this.Close();
+                        break;
+
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void rtbEditor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            cambiosRealizados = true;
+        }
+    }
+}
